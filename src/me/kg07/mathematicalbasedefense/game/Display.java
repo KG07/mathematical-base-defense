5f96d00452247d26a4dfcd715036b4c24c3e6568
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 14:02:09 +0700

    dahnfudahfiudafiudsfuiioqwofqeouifoiuqeufqiuohqfeouihuihfeqiuhefuhiefwdygfaysuyfadsyudfasyguydgsfydausgf

---
+++ src/me/kg07/mathematicalbasedefense/game/Display.java
@@ -0,0 +1,382 @@
+package me.kg07.mathematicalbasedefense.game;
+
+/*
+ * Display Class for MathematicalBaseDefense
+ * @author  mistertfy64
+ * @version 0.0.1-alpha
+ * @since   2020-11-15 or something i cant remember
+ *
+ */
+
+import me.kg07.mathematicalbasedefense.game.userinterface.ImageButton;
+import me.kg07.mathematicalbasedefense.launcher.LauncherWindow;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTreeUI;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Date;
+
+//i dont like mdes and phuttipong lol
+
+public class Display extends JFrame implements MouseListener {
+
+    /*
+     *
+     * Register images here!
+     *
+     */
+
+    //images
+    //title screen
+    BufferedImage singlePlayerButtonImage;
+    BufferedImage multiPlayerButtonImage;
+    BufferedImage optionsButtonImage;
+    BufferedImage quitButtonImage;
+    BufferedImage schoolLogoImage;
+    BufferedImage nationalSoftwareContestLogoImage;
+
+
+    //array list panels
+    //title screen
+    public static ArrayList<JPanel> titlePanels = new ArrayList<JPanel>();
+    public static ArrayList<Image> titlePanelImages = new ArrayList<Image>();
+    public static ArrayList<String> titlePanelNames = new ArrayList<String>();
+
+
+    private Canvas canvas;
+
+    public Color backgroundColor = new Color(238, 238, 238);
+
+
+    //state
+    public enum State {
+        TITLE_SCREEN,
+        SINGLEPLAYER_SCREEN,
+        MULTIPLAYER_SCREEN,
+        OPTIONS_SCREEN,
+    }
+    public static State currentState = State.TITLE_SCREEN;
+
+
+    public JFrame displayWindow = new JFrame();
+
+    GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+
+    private final int mainMonitorWidth = graphicsDevice.getDisplayMode().getWidth();
+    private final int mainMonitorHeight = graphicsDevice.getDisplayMode().getHeight();
+    public int windowWidth, windowHeight;
+
+
+    //declare fonts
+    Font computerModernFont = null;
+    Font computerModernFont12Pixels = null;
+    Font computerModernFont16Pixels = null;
+    Font computerModernFont20Pixels = null;
+    Font computerModernFont24Pixels = null;
+    Font computerModernFont32Pixels = null;
+    Font computerModernFont48Pixels = null;
+    Font computerModernFont72Pixels = null;
+
+    public Display(int resolutionWidth, int resolutionHeight, boolean fullscreen) {
+
+        try {
+            // sorry for doing this lol
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            computerModernFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/me/kg07/mathematicalbasedefense/game/assets/fonts/computermodern.ttf")).deriveFont(12f);
+            computerModernFont12Pixels = computerModernFont.deriveFont(12f);
+            computerModernFont16Pixels = computerModernFont.deriveFont(16f);
+            computerModernFont20Pixels = computerModernFont.deriveFont(20f);
+            computerModernFont24Pixels = computerModernFont.deriveFont(24f);
+            computerModernFont32Pixels = computerModernFont.deriveFont(32f);
+            computerModernFont48Pixels = computerModernFont.deriveFont(48f);
+            computerModernFont72Pixels = computerModernFont.deriveFont(72f);
+
+            ge.registerFont(computerModernFont);
+            ge.registerFont(computerModernFont12Pixels);
+            ge.registerFont(computerModernFont16Pixels);
+            ge.registerFont(computerModernFont20Pixels);
+            ge.registerFont(computerModernFont24Pixels);
+            ge.registerFont(computerModernFont32Pixels);
+            ge.registerFont(computerModernFont48Pixels);
+            ge.registerFont(computerModernFont72Pixels);
+
+
+            displayWindow = new JFrame();
+
+
+            displayWindow.setTitle("Mathematical Base Defense v0.0.1-alpha");
+            displayWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            displayWindow.setResizable(false);
+
+            windowWidth = resolutionWidth;
+            windowHeight = resolutionHeight;
+
+            canvas = new Canvas();
+            canvas.setPreferredSize(new Dimension(resolutionWidth, resolutionHeight));
+            canvas.addMouseListener(this);
+
+            //set display's properties
+
+            //If fullscreen = true, resolutionWidth and resolutionHeight are ignored.
+            if (fullscreen) {
+                setToFullScreen(displayWindow, graphicsDevice);
+            } else {
+                setSize(resolutionWidth, resolutionHeight);
+            }
+
+            //add stuff to display
+            displayWindow.add(canvas);
+            displayWindow.pack();
+
+            canvas.createBufferStrategy(3);
+
+            //show display
+            displayWindow.setLocationRelativeTo(null);
+            displayWindow.setVisible(true);
+
+            BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+            initializeUserInterfaceComponents(bufferStrategy);
+
+            System.out.println("Display Creation Complete");
+
+        } catch (Exception exception) {
+            displayWindow.dispose();
+            new ErrorWindow();
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent){
+        int xCoordinateOfClickedPosition = mouseEvent.getX();
+        int yCoordinateOfClickedPosition = mouseEvent.getY();
+        System.out.println("Mouse Clicked at " + "(" + xCoordinateOfClickedPosition + ", " + yCoordinateOfClickedPosition + ")");
+        switch (currentState){
+            case TITLE_SCREEN: {
+                    for (int i = 0; i < titlePanels.size(); i++){
+                        if (checkIfPointIsInRectangle(titlePanels.get(i), xCoordinateOfClickedPosition, yCoordinateOfClickedPosition)){
+                            switch (titlePanelNames.get(i)){
+                                case "singlePlayerButtonBounds" : {
+                                    currentState = State.SINGLEPLAYER_SCREEN;
+                                    break;
+                                }
+                                case "multiPlayerButtonBounds" : {
+                                    currentState = State.MULTIPLAYER_SCREEN;
+                                    break;
+                                }
+                                case "optionsButtonBounds" : {
+                                    currentState = State.OPTIONS_SCREEN;
+                                    break;
+                                }
+                                case "quitButtonBounds" : {
+                                    displayWindow.dispose();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+            case SINGLEPLAYER_SCREEN: {
+                    break;
+                }
+
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+    }
+
+
+
+    public void setToFullScreen(JFrame displayToMakeFullScreen, GraphicsDevice graphicsDevice) {
+        graphicsDevice.setFullScreenWindow(displayToMakeFullScreen);
+    }
+
+    public void render(Game game) {
+
+        BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+        Graphics2D canvasGraphics2D = (Graphics2D) bufferStrategy.getDrawGraphics();
+        Graphics2D imageButtonGraphics2D = (Graphics2D) bufferStrategy.getDrawGraphics();
+
+
+        //get canvas
+        canvasGraphics2D.setColor(backgroundColor);
+        canvasGraphics2D.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        //actually rendering stuff
+        switch (currentState){
+            case TITLE_SCREEN: {
+                for (int i = 0; i < titlePanels.size(); i++){
+                    imageButtonGraphics2D.drawImage(titlePanelImages.get(i), titlePanels.get(i).getX(), titlePanels.get(i).getY(), titlePanels.get(i).getWidth(), titlePanels.get(i).getHeight(), null);
+                }
+                break;
+            }
+        }
+
+
+
+
+
+        canvasGraphics2D.dispose();
+        bufferStrategy.show();
+    }
+
+
+
+
+
+
+
+
+
+
+    /*
+     * Finds the optimal x/y coordinate to draw centered text given the bounds, the text.
+     *
+     * @param graphics2D The graphics
+     * @param text The text
+     *
+     * I mean isn't that obvious???
+     *
+     */
+    private int getOptimalCoordinateToDrawCenteredText(Graphics2D graphics2D, JPanel bounds, String text, char coordinateToReturn) {
+        FontMetrics fontMetrics = graphics2D.getFontMetrics();
+        if (coordinateToReturn == 'x') {
+            return bounds.getX() + (bounds.getWidth() - fontMetrics.stringWidth(text)) / 2;
+        } else {
+            return bounds.getY() + (bounds.getHeight() - fontMetrics.getHeight()) / 2 + fontMetrics.getAscent();
+        }
+    }
+
+
+    /*
+     * Checks if a point is in a rectangle given the corners, and the coordinates of the point.
+     *
+     *
+     * I mean isn't that obvious???
+     *
+     */
+    private boolean checkIfPointIsInRectangle(JPanel panel, int px, int py){
+        return px > panel.getX() && px < panel.getX() + panel.getWidth() && py > panel.getY() && py < panel.getY() + panel.getHeight();
+    }
+
+    //Run when the single player button is pressed on the title screen
+    private void singlePlayerButtonPressed(){
+        currentState = State.SINGLEPLAYER_SCREEN;
+    }
+
+    /*
+    private void initializeUserInterfaceComponents(){
+
+        //setting properties
+
+        //text
+        //title text
+        titleTextLabelBounds.setBounds(0, 0, windowWidth, (int) (windowHeight * 0.175));
+        titleTextLabelGraphics2D.setFont(computerModernFont72Pixels);
+        // titleTextLabelGraphics2D.scale(windowWidth/1920d, windowHeight/1080d);
+        titleTextLabel.setText("Mathematical Base Defense");
+        titleTextLabelBounds.add(titleTextLabel);
+
+
+        //buttons
+        //singleplayer button
+        singlePlayerButtonBounds.setBounds((int) (windowWidth * 0.34375), (int) (windowHeight * 0.25), (600 * windowWidth / 1920),  (100 * windowHeight / 1080));
+        singlePlayerButton.setImage(singlePlayerButtonImage);
+        //multiplayer button
+        multiPlayerButtonBounds.setBounds((int) (windowWidth * 0.34375), (int) (windowHeight * 0.4), (600 * windowWidth / 1920),  (100 * windowHeight / 1080));
+        multiPlayerButton.setImage(multiPlayerButtonImage);
+        //options button
+        optionsButtonBounds.setBounds((int) (windowWidth * 0.34375), (int) (windowHeight * 0.55),  (600 * windowWidth / 1920),  (100 * windowHeight / 1080));
+        optionsButton.setImage(optionsButtonImage);
+        //quit button
+        quitButtonBounds.setBounds((int) (windowWidth * 0.34375), (int) (windowHeight * 0.7), (600 * windowWidth / 1920), (100 * windowHeight / 1080));
+        quitButton.setImage(quitButtonImage);
+
+        //add bounds to arraylist
+        //title screen
+        if (!(titlePanelNames.contains("singlePlayerButtonBounds"))){
+            titlePanels.add(singlePlayerButtonBounds);
+            titlePanelNames.add(singlePlayerButtonBounds.getName());
+        }
+
+        if (!(titlePanelNames.contains("multiPlayerButtonBounds"))){
+            titlePanels.add(multiPlayerButtonBounds);
+            titlePanelNames.add(multiPlayerButtonBounds.getName());
+        }
+
+
+        if (!(titlePanelNames.contains("optionsButtonBounds"))){
+            titlePanels.add(optionsButtonBounds);
+            titlePanelNames.add(optionsButtonBounds.getName());
+        }
+
+        if (!(titlePanelNames.contains("quitButtonBounds"))){
+            titlePanels.add(quitButtonBounds);
+            titlePanelNames.add(quitButtonBounds.getName());
+        }
+
+    }
+     */
+
+    private void initializeUserInterfaceComponents(BufferStrategy bufferStrategy){
+
+
+
+        try {
+
+            //images that aren't affected by selected language
+            schoolLogoImage = ImageIO.read(new File("src/me/kg07/mathematicalbasedefense/game/assets/images/schoollogo.png"));
+            nationalSoftwareContestLogoImage = ImageIO.read(new File("src/me/kg07/mathematicalbasedefense/game/assets/images/nationalsoftwarecontestlogo.png"));
+
+            singlePlayerButtonImage = ImageIO.read(new File("src/me/kg07/mathematicalbasedefense/game/assets/images/singleplayerbutton-en.png"));
+            multiPlayerButtonImage = ImageIO.read(new File("src/me/kg07/mathematicalbasedefense/game/assets/images/multiplayerbutton-en.png"));
+            optionsButtonImage = ImageIO.read(new File("src/me/kg07/mathematicalbasedefense/game/assets/images/optionsbutton-en.png"));
+            quitButtonImage = ImageIO.read(new File("src/me/kg07/mathematicalbasedefense/game/assets/images/quitbutton-en.png"));
+
+        } catch (Exception exception) {
+            displayWindow.dispose();
+            new ErrorWindow();
+        }
+
+
+
+        //title screen: text
+
+        //title screen: buttons
+        ImageButton singlePlayerButton = new ImageButton((int) (windowWidth * 0.34375), (int) (windowHeight * 0.25), (singlePlayerButtonImage.getWidth() * windowWidth / 1920), (singlePlayerButtonImage.getHeight() * windowHeight / 1080), singlePlayerButtonImage, "singlePlayerButton", "singlePlayerButtonBounds", "titleScreen", bufferStrategy);
+        ImageButton multiPlayerButton = new ImageButton((int) (windowWidth * 0.34375), (int) (windowHeight * 0.4), (multiPlayerButtonImage.getWidth() * windowWidth / 1920), (multiPlayerButtonImage.getHeight() * windowHeight / 1080), multiPlayerButtonImage, "multiPlayerButton", "multiPlayerButtonBounds", "titleScreen", bufferStrategy);
+        ImageButton optionsButton = new ImageButton((int) (windowWidth * 0.34375), (int) (windowHeight * 0.55), (optionsButtonImage.getWidth() * windowWidth / 1920), (optionsButtonImage.getHeight() * windowHeight / 1080), optionsButtonImage, "optionsButton", "optionsButtonBounds", "titleScreen", bufferStrategy);
+        ImageButton quitButton = new ImageButton((int) (windowWidth * 0.34375), (int) (windowHeight * 0.7), (quitButtonImage.getWidth() * windowWidth / 1920), (quitButtonImage.getHeight() * windowHeight / 1080), quitButtonImage, "quitButton", "quitButtonBounds", "titleScreen", bufferStrategy);
+        ImageButton schoolLogo = new ImageButton((int) (windowWidth * 0.005), (int) (windowHeight * 0.9), (schoolLogoImage.getWidth() * windowWidth / 1920), (schoolLogoImage.getHeight() * windowHeight / 1080), schoolLogoImage, "schoolLogo", "schoolLogoBounds", "titleScreen", bufferStrategy);
+        ImageButton nationalSoftwareContestLogo = new ImageButton((int) (windowWidth * 0.04), (int) (windowHeight * 0.9), (nationalSoftwareContestLogoImage.getWidth() * windowWidth/ 1920), (nationalSoftwareContestLogoImage.getHeight() * windowHeight / 1080), nationalSoftwareContestLogoImage, "nationalSoftwareContestLogo", "nationalSoftwareContestLogoBounds", "titleScreen", bufferStrategy);
+    }
+}
+
+
+
+
---
+++ src/me/kg07/mathematicalbasedefense/launcher/LauncherWindow.java
@@ -0,0 +1,274 @@
+package me.kg07.mathematicalbasedefense.launcher;
+
+
+/*
+ * Launcher for MathematicalBaseDefense
+ * @author  mistertfy64
+ * @version 0.0.1-alpha
+ * @since   2020-11-15 or something i cant remember
+ *
+ */
+
+
+import me.kg07.mathematicalbasedefense.game.Display;
+import me.kg07.mathematicalbasedefense.game.Game;
+import me.kg07.mathematicalbasedefense.game.Loop;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class LauncherWindow implements ActionListener {
+
+    //declare frame
+    JFrame launcherWindow;
+    //declare panels
+    JPanel titlePanel;
+    JPanel launchGameButtonPanel;
+    JPanel versionPanel;
+    JPanel resolutionSelectionBoxPanel;
+    //declare labels
+    JLabel titleLabel;
+    JLabel launchGameButtonTextLabel;
+    JLabel versionLabel;
+    //declare combo boxes
+    JComboBox resolutionSelectionBox;
+    //declare buttons
+    JButton launchGameButton;
+
+
+    String[] resolutions = {"800x600", "960x720", "1024x768", "1280x960", "1400x1050", "1440x1080", "1600x1200", "1024x576", "1152x648", "1280x720", "1366x768", "1600x900", "1920x1080"};
+
+    int selectedResolutionWidth = 1920;
+    int selectedResolutionHeight = 1080;
+    boolean fullScreen = false;
+
+    public LauncherWindow(){
+        //declare fonts
+        Font computerModernFont = null;
+        Font computerModernFont12Pixels = null;
+        Font computerModernFont16Pixels = null;
+        Font computerModernFont20Pixels = null;
+        Font computerModernFont24Pixels = null;
+        Font computerModernFont32Pixels = null;
+        Font computerModernFont48Pixels = null;
+        try {
+            // sorry for doing this lol
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            computerModernFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/me/kg07/mathematicalbasedefense/launcher/assets/computermodern.ttf")).deriveFont(12f);
+            computerModernFont12Pixels = computerModernFont.deriveFont(12f);
+            computerModernFont16Pixels = computerModernFont.deriveFont(16f);
+            computerModernFont20Pixels = computerModernFont.deriveFont(20f);
+            computerModernFont24Pixels = computerModernFont.deriveFont(24f);
+            computerModernFont32Pixels = computerModernFont.deriveFont(32f);
+            computerModernFont48Pixels = computerModernFont.deriveFont(48f);
+            //register fonts
+            ge.registerFont(computerModernFont);
+            ge.registerFont(computerModernFont12Pixels);
+            ge.registerFont(computerModernFont16Pixels);
+            ge.registerFont(computerModernFont20Pixels);
+            ge.registerFont(computerModernFont24Pixels);
+            ge.registerFont(computerModernFont32Pixels);
+            ge.registerFont(computerModernFont48Pixels);
+
+
+            //declare variables
+
+
+
+
+
+
+
+
+
+            //make window
+            launcherWindow = new JFrame();
+            //declare panels
+            titlePanel = new JPanel();
+            launchGameButtonPanel = new JPanel();
+            versionPanel = new JPanel();
+            resolutionSelectionBoxPanel = new JPanel();
+            //declare labels
+            titleLabel = new JLabel();
+            launchGameButtonTextLabel = new JLabel();
+            versionLabel = new JLabel();
+            //declare combobox
+            resolutionSelectionBox = new JComboBox(resolutions);
+            //declare buttons
+            launchGameButton = new JButton();
+
+
+            //set frame properties
+            launcherWindow.setSize(800, 600);
+            launcherWindow.setTitle("Mathematical Base Defense Launcher");
+            launcherWindow.setResizable(false);
+            launcherWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //set label properties
+            titleLabel.setFont(computerModernFont48Pixels);
+            titleLabel.setText("Mathematical Base Defense");
+
+            versionLabel.setFont(computerModernFont16Pixels);
+            versionLabel.setText("Version 0.0.1-alpha");
+
+            launchGameButtonTextLabel.setFont(computerModernFont20Pixels);
+            launchGameButtonTextLabel.setText("Launch Mathematical Base Defense");
+            launchGameButtonTextLabel.setHorizontalTextPosition(JLabel.CENTER);
+            launchGameButtonTextLabel.setVerticalTextPosition(JLabel.CENTER);
+
+
+            //set button properties
+            launchGameButton.add(launchGameButtonTextLabel);
+
+            //set combo box
+            resolutionSelectionBox.setSelectedIndex(resolutions.length - 1);
+
+
+            //add event listeners
+            launchGameButton.addActionListener(this);
+            resolutionSelectionBox.addActionListener(this);
+
+            // set panel properties
+            titlePanel.add(titleLabel);
+            titlePanel.setBounds(100, 15, 600, 50);
+            versionPanel.add(versionLabel);
+            versionPanel.setBounds(250, 70, 300, 25);
+            launchGameButtonPanel.add(titlePanel);
+            launchGameButtonPanel.add(launchGameButton);
+            launchGameButtonPanel.setBounds(200, 500, 400, 50);
+            launchGameButtonPanel.setPreferredSize(new Dimension(600, 600));
+            resolutionSelectionBoxPanel.add(resolutionSelectionBox);
+            resolutionSelectionBoxPanel.setBounds(100, 500, 100, 35);
+
+
+            //set stuff
+            launcherWindow.add(titlePanel);
+            launcherWindow.add(versionPanel);
+            launcherWindow.add(launchGameButtonPanel);
+            launcherWindow.add(resolutionSelectionBoxPanel);
+            launcherWindow.setLayout(null);
+
+
+            //idk what is this for
+            changeStringToNewResolution("1920x1080");
+
+            //show the frame
+            launcherWindow.setVisible(true);
+
+            
+            
+        } catch (IOException | FontFormatException e) {
+            JFrame errorWindow = new JFrame();
+            JLabel label = new JLabel();
+            errorWindow.setSize(500, 100);
+            errorWindow.setResizable(false);
+            errorWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            label.setText("An error has occurred while trying to launch the launcher.");
+            errorWindow.add(label);
+            errorWindow.setVisible(true);
+            e.printStackTrace();
+        }
+
+
+
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent){
+            //launch game button
+        if (actionEvent.getSource() == launchGameButton){
+            launcherWindow.dispose();
+            new Thread(new Loop(new Game(selectedResolutionWidth, selectedResolutionHeight, false))).start();
+
+            //resolution selection box
+        } else if (actionEvent.getSource() == resolutionSelectionBox){
+            JComboBox resolutionSelectionBox = (JComboBox) actionEvent.getSource();
+            String selectedResolution = (String) resolutionSelectionBox.getSelectedItem();
+            changeStringToNewResolution(selectedResolution);
+        }
+    }
+
+
+    //AT LEAST I TRIED SO SHUT UP
+    //this sucks, am i right?
+    private void changeStringToNewResolution(String selectedResolution){
+        for (int i = 0; i < resolutions.length; i++){
+            if (selectedResolution.equals(resolutions[i])){
+                switch (i){
+                    case 0: {
+                        selectedResolutionWidth = 800;
+                        selectedResolutionHeight = 600;
+                        break;
+                    }
+                    case 1: {
+                        selectedResolutionWidth = 960;
+                        selectedResolutionHeight = 720;
+                        break;
+                    }
+                    case 2: {
+                        selectedResolutionWidth = 1024;
+                        selectedResolutionHeight = 768;
+                        break;
+                    }
+                    case 3: {
+                        selectedResolutionWidth = 1280;
+                        selectedResolutionHeight = 960;
+                        break;
+                    }
+                    case 4: {
+                        selectedResolutionWidth = 1400;
+                        selectedResolutionHeight = 1050;
+                        break;
+                    }
+                    case 5: {
+                        selectedResolutionWidth = 1440;
+                        selectedResolutionHeight = 1080;
+                        break;
+                    }
+                    case 6: {
+                        selectedResolutionWidth = 1600;
+                        selectedResolutionHeight = 1200;
+                        break;
+                    }
+                    case 7: {
+                        selectedResolutionWidth = 1024;
+                        selectedResolutionHeight = 576;
+                        break;
+                    }
+                    case 8: {
+                        selectedResolutionWidth = 1152;
+                        selectedResolutionHeight = 648;
+                        break;
+                    }
+                    case 9: {
+                        selectedResolutionWidth = 1280;
+                        selectedResolutionHeight = 720;
+                        break;
+                    }
+                    case 10: {
+                        selectedResolutionWidth = 1366;
+                        selectedResolutionHeight = 768;
+                        break;
+                    }
+                    case 11: {
+                        selectedResolutionWidth = 1600;
+                        selectedResolutionHeight = 900;
+                        break;
+                    }
+                    case 12: {
+                        selectedResolutionWidth = 1920;
+                        selectedResolutionHeight = 1080;
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+
+}
